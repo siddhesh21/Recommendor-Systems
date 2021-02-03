72bb51e255753fddb4d66294da97a63bfe1b5fea
Robzlegz <robzlegz@gmail.com>
2021-02-03 23:26:06 +0200

    fixing styling

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -11,7 +11,6 @@ const HomeScreen = () => {
             <Nav />
             <Banner />
             <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
-            
             <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
             <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
             <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
