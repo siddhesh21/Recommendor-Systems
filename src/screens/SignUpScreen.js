94e1c828b971f3c9eadeb915acde4cf8019aef49
Robzlegz <robzlegz@gmail.com>
2021-02-04 21:32:08 +0200

    user state update

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { auth } from '../firebase';
 import "./SignUpScreen.css";
 
 const SignUpScreen = () => {
 
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
     const register = (e) => {
         e.preventDefault();
-
-    }
+        auth.createUserWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) => {
+            console.log(authUser)
+        }).catch((error) => alert(error.message))
+    };
 
     const signIn = (e) => {
         e.preventDefault();
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) => {
+            console.log(authUser)
+        }).catch((error) => alert(error.message))
     }
  
     return (
         <div className="signUpScreen">
             <form>
                 <h1>Sign In</h1>
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="password"/>
+                <input ref={emailRef} type="email" placeholder="Email"/>
+                <input ref={passwordRef} type="password" placeholder="password"/>
                 <button onClick={signIn} type="submit">Sign In</button>
                 
             </form>
