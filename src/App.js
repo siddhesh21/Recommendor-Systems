2351c0ba25f8d4fc1490e68f901c21121d437c85
Robzlegz <robzlegz@gmail.com>
2021-02-04 20:30:19 +0200

    setting up firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomeScreen from './HomeScreen';
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA3ntjhoMFlBtkXLam2vu2wProaTvwi64I",
+    authDomain: "instagram-clone-fc2ce.firebaseapp.com",
+    projectId: "instagram-clone-fc2ce",
+    storageBucket: "instagram-clone-fc2ce.appspot.com",
+    messagingSenderId: "639165461153",
+    appId: "1:639165461153:web:9750aa95bd54c697fdf6e0",
+    measurementId: "G-8K0H85QJJ3"
+};
