68f8ac77b35d4ae9602d27d461130d79e94122cc
Robzlegz <robzlegz@gmail.com>
2021-02-04 23:52:25 +0200

    adding new features

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../features/userSlice';
 import { auth } from '../firebase';
 import Nav from '../Nav';
 import "./ProfileScreen.css"
+import EditIcon from '@material-ui/icons/Edit';
 
 const ProfileScreen = () => {
 
     const user = useSelector(selectUser);
+    const [profileUrl, setProfileUrl] = useState("");
+
+    const toggleProfileForm = (e) => {
+        e.preventDefault();
+        document.querySelector(".profileImageForm").classList.toggle("hiddenProfileUrlForm");
+    }
 
     return (
         <div className="profileScreen">
             <Nav />
             <div className="profileScreenBody">
                 <h1>Edit profile</h1>
+                <form className="profileImageForm hiddenProfileUrlForm">
+                    <input onChange={(e) => setProfileUrl(e.target.value)} className="profileImageUrlInput" type="text" placeholder="Profile Image Url"/>
+                    <button type="submit" onClick={toggleProfileForm}>Enter</button>
+                </form>
                 <div className="profileScreenInfo">
-                    <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="asd"/>
+                    <div className="profileImageContainer">
+                        <img src={`${!profileUrl ? "https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" : profileUrl}`} alt="asd"/>
+                        <div onClick={toggleProfileForm} className="imageOverlay">
+                            <EditIcon />
+                        </div>
+                    </div>
                     <div className="profileScreenDetails">
                         <h2>{user.email}</h2>
                         <div className="profileScreenPlans">
